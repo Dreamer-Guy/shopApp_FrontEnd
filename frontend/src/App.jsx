5aec69de3cd816cfc29671a496ac9f97a5ecf79a
Duc Toan <22120376@student.hcmus.edu.vn>
2024-11-21 17:42:46 +0700

    setup Shadcn UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
