d1a748d9f7e5e3c300e232fff69225ede749867a
Duc Toan <22120376@student.hcmus.edu.vn>
2024-12-31 12:14:08 +0700

    some fix

--- src/components/shop/footer.jsx
+++ src/components/shop/footer.jsx
@@ -87,11 +87,11 @@ const UserFooter = () => {
             </div>
         </footer>
         
-        <div class="bg-gray-800 py-4">
-            <div class="container flex items-center justify-between">
-                <p class="text-white">&copy; TailCommerce - All Right Reserved</p>
+        <div className="bg-gray-800 py-4">
+            <div className="container flex items-center justify-between">
+                <p className="text-white">&copy; TailCommerce - All Right Reserved</p>
                 <div>
-                    <img src="/assets/images/methods.png" alt="methods" class="h-5"/>
+                    <img src="/assets/methods.png" alt="methods" className="h-5"/>
                 </div>
             </div>
         </div>
