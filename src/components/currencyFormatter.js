163915c70bb9a7e7c5c496c38b32d29b08942802
UYTwlight <sontranminh82@gmail.com>
2025-01-12 14:42:11 +0700

    change currency format

--- src/components/currencyFormatter.js
+++ src/components/currencyFormatter.js
@@ -1,18 +1,27 @@
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 3, // Ensure 3 decimal places
-    maximumFractionDigits: 3, // Ensure 3 decimal places
-  });
-  
-  const formatWithSpace = (number) => {
-    // Format the number using Intl.NumberFormat
-    const formatted = formatter.format(number);
-    
-    // Replace the default thousand separator (comma) with a space
-    return formatted.replace(/,/g, ' ');
-  };
-  
-  // Test cases
-  //console.log(formatWithSpace(1000.3));  // Output: "$1 000.300"
-  //console.log(formatWithSpace(1234567.89)); // Output: "$1 234 567.890"
+const numberFormatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 3,
+  maximumFractionDigits: 3,
+});
+
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 3,
+  maximumFractionDigits: 3,
+});
+
+const formatNumber = (number) => {
+  const formatted = numberFormatter.format(number);
+  return formatted.replace(/,/g, ' ');
+};
+
+const formatCurrency = (number) => {
+  const formatted = currencyFormatter.format(number);
+  return formatted.replace(/,/g, ' ');
+};
+
+// Export both functions
+//use
+//console.log(formatNumber(1234567.89));  // "1 234 567.890"
+//console.log(formatCurrency(1234567.89)); // "$1 234 567.890"
+export { formatNumber, formatCurrency };
